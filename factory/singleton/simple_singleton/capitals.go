1a48d6e976bde94168aaf94b0c4048700f015c8d
AkshatAsthana1412 <akshatsagittarius96@gmail.com>
2025-06-11 23:25:02 +0530

    Fix DIP in singleton

--- factory/singleton/simple_singleton/capitals.go
+++ factory/singleton/simple_singleton/capitals.go
@@ -38,6 +38,26 @@ func readFile(filename string) (map[string]int, error) {
 	return popMap, nil
 }
 
+// to deal with violation of DIP by singleton pattern, we introduce the Database interface
+type Database interface {
+	GetPopulation(city string) (int, error)
+}
+
+type DummyDatabase struct {
+	dummyPopulations map[string]int
+}
+
+func (db *DummyDatabase) GetPopulation(city string) (int, error) {
+	// implementing lazy instantiation
+	if len(db.dummyPopulations) == 0 {
+		db.dummyPopulations = map[string]int{
+			"alpha": 10,
+			"beta":  5,
+			"gamma": 1}
+	}
+	return db.dummyPopulations[city], nil
+}
+
 // note that it starts with lowercase 's', we don't want it to be accessed directly
 // i.e. it's private to this package, use a factory function to initialise this
 // the singletonDatabase should only be instantiated once
@@ -50,7 +70,7 @@ var once sync.Once // construct used to initialise something only once,
 
 var instance *singletonDatabase
 
-// GetPopulation and GetSingletonDatabase violate the dependancy inversion principle
+// GetPopulation violate the dependancy inversion principle
 // because they define rigid methods to access the data. We need to have abstractions/interfaces,
 // this helps in unit tests where you may use a dummy database
 func (db *singletonDatabase) GetPopulation(city string) (int, error) {
@@ -72,8 +92,18 @@ func GetSingletonDatabase() *singletonDatabase {
 	return instance
 }
 
+func GetTotalPopulation(db Database, cities []string) int {
+	cnt := 0
+	for _, city := range cities {
+		pop, _ := db.GetPopulation(city)
+		cnt += pop
+	}
+	return cnt
+}
+
 func main() {
-	db := GetSingletonDatabase()
-	pop, _ := db.GetPopulation("Tokyo")
-	fmt.Printf("Population of Tokyo: %d\n", pop)
+	// db := GetSingletonDatabase()
+	test_pop := GetTotalPopulation(&DummyDatabase{}, []string{"alpha", "gamma"})
+	ok := test_pop == 11
+	fmt.Printf("Test pass?: %t", ok)
 }

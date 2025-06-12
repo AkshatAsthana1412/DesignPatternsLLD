a5aeec8b05ee831a7376ed926d826e9d1ee09285
AkshatAsthana1412 <akshatsagittarius96@gmail.com>
2025-06-13 00:54:34 +0530

    Refactor and fix iteration issue of List

--- observer/example2/traffic_mgmt.go
+++ observer/example2/traffic_mgmt.go
@@ -27,11 +27,12 @@ func (t *Traffic_Management) Unsubscribe(o ...Observer) {
 }
 
 func (t *Traffic_Management) NotifyAllObservers() {
+	var toUnsubscribe []Observer
 	for observer := t.persons.Front(); observer != nil; observer = observer.Next() {
 		if p, ok := observer.Value.(*Person); ok {
 			if p.eligibilityCriteria.IsEligible(p) {
-				p.notify(fmt.Sprintf("Congrats %s! you're allowed to drive now.\n", p.getName()))
-				t.Unsubscribe(p)
+				p.notify(fmt.Sprintf("Congrats %s! you're allowed to drive now.", p.getName()))
+				toUnsubscribe = append(toUnsubscribe, p)
 			} else {
 				p.notify(fmt.Sprintf("%s you're not yet eligible to drive", p.getName()))
 			}
@@ -39,4 +40,5 @@ func (t *Traffic_Management) NotifyAllObservers() {
 			fmt.Println("Invalid observer type")
 		}
 	}
+	t.Unsubscribe(toUnsubscribe...)
 }
